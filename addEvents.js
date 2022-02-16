8dceffe332e9c9f6e7fa7e257be89b9bdfbf382f
Carlos Calderon <cdcalderon@gmail.com>
2022-02-15 21:19:29 -0600

    add file to track events in moralis

---
+++ addEvents.js
@@ -0,0 +1,28 @@
+const Moralis = require("moralis/node")
+require("dotenv").config()
+const contractAddresses = require("./constants/networkMapping.json")
+let chainId = process.env.chainId || 31337
+let moralisChainId = chainId == "31337" ? "1337" : chainId
+const contractAddress = contractAddresses[chainId]["NftMarketplace"][0]
+
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL
+const appId = process.env.NEXT_PUBLIC_APP_ID
+const masterKey = process.env.masterKey
+
+async function main() {
+    await Moralis.start({ serverUrl, appId, masterKey })
+    console.log(`Working with contrat address ${contractAddress}`)
+
+    if (listedResponse.success && canceledResponse.success && boughtResponse.success) {
+        console.log("Success! Database Updated with watching events")
+    } else {
+        console.log("Something went wrong...")
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
