13404f380b04ee1b678ee2127c93c0eedd74897a
Carlos Calderon <cdcalderon@gmail.com>
2022-02-16 23:24:55 -0600

    add NFT box component

---
+++ components/NFTBox.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react"
+import { useWeb3Contract, useMoralis } from "react-moralis"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import nftAbi from "../constants/BasicNft.json"
+
+export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {}
