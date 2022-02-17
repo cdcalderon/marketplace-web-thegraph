a17f2697a2fc9cb5a297394eda0e1de884ad2db9
Carlos Calderon <cdcalderon@gmail.com>
2022-02-16 23:40:47 -0600

    implement updateUI function

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -3,4 +3,35 @@ import { useWeb3Contract, useMoralis } from "react-moralis"
 import nftMarketplaceAbi from "../constants/NftMarketplace.json"
 import nftAbi from "../constants/BasicNft.json"
 
-export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {}
+// BasicNFt ->  function tokenURI(uint256 tokenId) public view override returns (string memory) { }
+const { runContractFunction: getTokenURI } = useWeb3Contract({
+    abi: nftAbi,
+    contractAddress: nftAddress,
+    functionName: "tokenURI",
+    params: {
+        tokenId: tokenId,
+    },
+})
+
+export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    async function updateUI() {
+        const tokenURI = await getTokenURI()
+        console.log(`The TokenURI is ${tokenURI}`)
+        if (tokenURI) {
+            // IPFS Gateway: A server that will return IPFS files from a "normal" URL.
+            const requestURL = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            const tokenURIResponse = await (await fetch(requestURL)).json()
+            const imageURI = tokenURIResponse.image
+            const imageURIURL = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            setImageURI(imageURIURL)
+            setTokenName(tokenURIResponse.name)
+            setTokenDescription(tokenURIResponse.description)
+        }
+    }
+}
