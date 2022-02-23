9d863f4d6a9f44f527c6a0fc474a25163ed27cf8
Carlos Calderon <cdcalderon@gmail.com>
2022-02-22 20:37:32 -0600

    add display listing

--- components/UpdateListingModal.js
+++ components/UpdateListingModal.js
@@ -1,11 +1,29 @@
 import { Modal, Input } from "web3uikit"
 
 export default function UpdateListingModal({ isVisible, onClose }) {
+    const dispatch = useNotification()
+
+    const [priceToUpdateListingWith, setPriceToUpdateListingWith] = useState(0)
+
+    const handleUpdateListingSuccess = () => {
+        dispatch({
+            type: "success",
+            message: "listing updated",
+            title: "Listing updated - please refresh (and move blocks)",
+            position: "topR",
+        })
+        onClose && onClose()
+        setPriceToUpdateListingWith("0")
+    }
+
     return (
         <Input
             label="Update listing price in L1 Currency (ETH)"
             name="New listing price"
             type="number"
+            onChange={(event) => {
+                setPriceToUpdateListingWith(event.target.value)
+            }}
         />
     )
 }
