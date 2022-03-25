ca0d9c427b207ded67ea9bc519872440243c516c
Carlos Calderon <cdcalderon@gmail.com>
2022-03-25 14:40:27 -0500

    fix Header export issue

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
-import Link from "next/link"
 import { ConnectButton } from "web3uikit"
+import Link from "next/link"
 
-export const Header = () => {
+export default function Header() {
     return (
         <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
             <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
