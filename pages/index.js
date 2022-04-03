c400cfa06dbca64714225a0fd4004ad0d2c156b2
Carlos Calderon <cdcalderon@gmail.com>
2022-04-02 22:56:39 -0500

    test NFT cart

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
+import NFTBox from "../components/NFTBox"
+import networkMapping from "../constants/networkMapping.json"
 
 export default function Home() {
-    return <div className={styles.container}>Hi Index!</div>
+    const { isWeb3Enabled, chainId } = useMoralis()
+    const chainString = chainId ? parseInt(chainId).toString() : "31337"
+    const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
+
+    return (
+        <div className="container mx-auto">
+            <div>Test NFT Cart</div>
+            <NFTBox price="123" />
+        </div>
+    )
 }
