d8f6cb083c93a4a8f59a1747f415ed9223c60bfa
Carlos Calderon <cdcalderon@gmail.com>
2022-04-04 00:35:35 -0500

    add form

--- pages/sell-nft.js
+++ pages/sell-nft.js
@@ -7,6 +7,31 @@ export default function Home() {
     const [proceeds, setProceeds] = useState("0")
     return (
         <div className={styles.container}>
+            <Form
+                data={[
+                    {
+                        name: "NFT Address",
+                        type: "text",
+                        inputWidth: "50%",
+                        value: "",
+                        key: "nftAddress",
+                    },
+                    {
+                        name: "Token ID",
+                        type: "number",
+                        value: "",
+                        key: "tokenId",
+                    },
+                    {
+                        name: "Price (in ETH)",
+                        type: "number",
+                        value: "",
+                        key: "price",
+                    },
+                ]}
+                title="Sell your NFT!"
+                id="Main Form"
+            />
             {proceeds != "0" ? <div>{proceeds}</div> : <div>No proceeds detected</div>}
         </div>
     )
