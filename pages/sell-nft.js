8a95a7460e6290aa45d5db13a82aee54d9f5f4ad
Carlos Calderon <cdcalderon@gmail.com>
2022-04-05 00:42:29 -0500

    add approve and list on submit functionality

--- pages/sell-nft.js
+++ pages/sell-nft.js
@@ -1,13 +1,49 @@
 import Head from "next/head"
+import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import { Form, useNotification, Button } from "web3uikit"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { ethers } from "ethers"
+import nftAbi from "../constants/BasicNft.json"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import networkMapping from "../constants/networkMapping.json"
+import { useEffect, useState } from "react"
 
 export default function Home() {
     const { chainId, account, isWeb3Enabled } = useMoralis()
     const chainString = chainId ? parseInt(chainId).toString() : "31337"
     const [proceeds, setProceeds] = useState("0")
+    const { runContractFunction } = useWeb3Contract()
+
+    async function approveAndList(data) {
+        console.log("Approving...")
+        const nftAddress = data.data[0].inputResult
+        const tokenId = data.data[1].inputResult
+        const price = ethers.utils.parseUnits(data.data[2].inputResult, "ether").toString()
+
+        const approveOptions = {
+            abi: nftAbi,
+            contractAddress: nftAddress,
+            functionName: "approve",
+            params: {
+                to: marketplaceAddress,
+                tokenId: tokenId,
+            },
+        }
+
+        await runContractFunction({
+            params: approveOptions,
+            onSuccess: (tx) => console.log(tx),
+            onError: (error) => {
+                console.log(error)
+            },
+        })
+    }
+
     return (
         <div className={styles.container}>
             <Form
+                onSubmit={approveAndList}
                 data={[
                     {
                         name: "NFT Address",
